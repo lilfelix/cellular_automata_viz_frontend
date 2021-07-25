ead2c63112d370069159fbd1ab0eb3f243f1daa3
Felix <felixlil@kth.se>
2021-07-25 18:27:40 +0200

    from old mbp

--- src/index.ts
+++ src/index.ts
@@ -4,30 +4,72 @@ import {
   WebGLRenderer,
   BoxGeometry,
   Mesh,
-  MeshBasicMaterial
+  MeshBasicMaterial,
+  LineBasicMaterial,
+  Vector3,
+  BufferGeometry,
+  Line
 } from 'three';
 
-const scene = new Scene();
-const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+(function main() {
+  const { scene, camera, renderer } = setup()
 
-const renderer = new WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+  const cube = createCube();
+  scene.add(cube);
 
-const geometry = new BoxGeometry();
-const material = new MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new Mesh(geometry, material);
-scene.add(cube);
+  //create a blue LineBasicMaterial
+  const material = new LineBasicMaterial({ color: 0x0000ff });
+  const points: Vector3[] = [];
+  points.push(new Vector3(- 10, 0, 0));
+  points.push(new Vector3(0, 10, 0));
+  points.push(new Vector3(10, 0, 0));
 
-camera.position.z = 5;
+  const geometry = new BufferGeometry().setFromPoints(points);
+  const line = new Line(geometry, material);
+  scene.add(line);
 
-const animate = function () {
-  requestAnimationFrame(animate);
+  let zoomingIn = true;
+  function animate() {
+    requestAnimationFrame(animate);
+    const actions = [
+      (o => o.rotation.x += 0.01),
+      (o => o.rotation.y += 0.01)
+    ];
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+    const r = camera.position.z / 10;
+    if (zoomingIn && r >= 1) {
+      camera.position.z -= 0.3
+      // camera.position.x += 0.01;
+    } else if (zoomingIn) {
+      zoomingIn = !zoomingIn;
+    } else if (!zoomingIn && r < 3) {
+      camera.position.z += 0.3
+      camera.position.x -= 0.03;
+    } else {
+      zoomingIn = !zoomingIn;
+    }
+    actions.forEach(a => a(cube));
+    renderer.render(scene, camera);
+  };
 
-  renderer.render(scene, camera);
-};
+  animate();
 
-animate();
+})();
+
+function setup() {
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  const renderer = new WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  camera.position.z = 30;
+
+  return { scene, camera, renderer };
+}
+
+function createCube() {
+  const geometry = new BoxGeometry();
+  const material = new MeshBasicMaterial({ color: 0x00ff00 });
+  return new Mesh(geometry, material);
+}
